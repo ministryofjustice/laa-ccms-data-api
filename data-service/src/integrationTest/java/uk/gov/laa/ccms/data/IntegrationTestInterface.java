0af37aefe04ca807dc2d5467a159d6e0361b60d3
farrell-m <michael.farrell@digital.justice.gov.uk>
2024-06-06 10:58:20 +0100

    add missing variable

--- data-service/src/integrationTest/java/uk/gov/laa/ccms/data/IntegrationTestInterface.java
+++ data-service/src/integrationTest/java/uk/gov/laa/ccms/data/IntegrationTestInterface.java
@@ -20,6 +20,7 @@ public interface IntegrationTestInterface {
     registry.add("spring.datasource.username", oracleContainerSingleton.getOracleContainer()::getUsername);
     registry.add("spring.datasource.password", oracleContainerSingleton.getOracleContainer()::getPassword);
 
+    registry.add("laa.ccms.springboot.starter.auth.authentication-header", () -> "Authorization");
     registry.add("laa.ccms.springboot.starter.auth.authorized-clients", () -> "[{\"name\":\"caab-ui\",\"roles\":[\"ALL\"],\"token\":\"d594f93f-e767-4b88-a9e9-2913441edfba\"}]");
     registry.add("laa.ccms.springboot.starter.auth.authorized-roles", () -> "[{\"name\":\"ALL\",\"URIs\":[\"/**\"]}]");
     registry.add("laa.ccms.springboot.starter.auth.unprotected-uris", () -> "[\"\"]");
