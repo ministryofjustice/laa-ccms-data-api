c47fab3120f085892cdc3026f8c75326be2631a4
Jamie Briggs <jamie.briggs@digital.justice.gov.uk>
2025-01-22 15:47:57 +0000

    CCMSPUI-382 TransactionStatusRepository updated to be able to search for case transactions
    Signed-off-by: Jamie Briggs <jamie.briggs@digital.justice.gov.uk>

--- data-service/src/main/java/uk/gov/laa/ccms/data/controller/CaseSearchController.java
+++ data-service/src/main/java/uk/gov/laa/ccms/data/controller/CaseSearchController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.laa.ccms.data.api.CasesApi;
 import uk.gov.laa.ccms.data.model.CaseDetails;
+import uk.gov.laa.ccms.data.model.TransactionStatus;
 import uk.gov.laa.ccms.data.service.CaseSearchService;
 
 /**
@@ -52,5 +53,10 @@ public class CaseSearchController implements CasesApi {
           officeId, pageable);
     return cases.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 
+  }
+
+  @Override
+  public ResponseEntity<TransactionStatus> getCaseTransactionStatus(String transactionRequestId) {
+    return null;
   }
 }
---
+++ data-service/src/main/java/uk/gov/laa/ccms/data/service/CaseService.java
@@ -0,0 +1,8 @@
+package uk.gov.laa.ccms.data.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CaseService {
+
+}
