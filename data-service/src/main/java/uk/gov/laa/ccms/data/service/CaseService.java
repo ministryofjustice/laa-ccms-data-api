035de43df8905f308852bebd6700f0f2a02b817c
Jamie Briggs <jamie.briggs@digital.justice.gov.uk>
2025-01-22 19:14:36 +0000

    CCMSPUI-382 Added javadocs to CaseService
    Signed-off-by: Jamie Briggs <jamie.briggs@digital.justice.gov.uk>

--- data-service/src/main/java/uk/gov/laa/ccms/data/service/CaseService.java
+++ data-service/src/main/java/uk/gov/laa/ccms/data/service/CaseService.java
@@ -8,6 +8,17 @@ import uk.gov.laa.ccms.data.mapper.TransactionStatusMapper;
 import uk.gov.laa.ccms.data.model.TransactionStatus;
 import uk.gov.laa.ccms.data.repository.TransactionStatusRepository;
 
+/**
+ * Service class responsible for handling case-related operations.
+ *
+ * <p>This class provides methods to retrieve transaction statuses for case-related
+ * transactions. It utilizes a repository for database access and a mapper for converting
+ * database entities to objects used in the application.</p>
+ *
+ * @see TransactionStatusRepository
+ * @see TransactionStatusMapper
+ * @author Jamie Briggs
+ */
 @Service
 @RequiredArgsConstructor
 public class CaseService {
@@ -15,7 +26,17 @@ public class CaseService {
   private final TransactionStatusRepository transactionStatusRepository;
   private final TransactionStatusMapper transactionStatusMapper;
 
-
+  /**
+   * Retrieves the transaction status for a given transaction ID. If the transaction
+   *     is not found, an empty {@code Optional} is returned.
+   *
+   * @param transactionId the unique identifier of the transaction whose status
+   *     is to be retrieved
+   * @return an {@code Optional} containing the {@link TransactionStatus} if found,
+   *     or an empty {@code Optional} if not found
+   * @throws ClientServiceException throws exception when there was an error found
+   *     with the associated transaction ID
+   */
   public Optional<TransactionStatus> getTransactionStatus(String transactionId) {
     List<uk.gov.laa.ccms.data.entity.TransactionStatus> userFuncStatus =
         transactionStatusRepository.findAllUserFunctionTransactionsByTransactionId(transactionId);
